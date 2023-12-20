3669156df4ae22d85e7f253f49712c5160f21f5d
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-12-20 21:16:09 +0300

    sanitizing the output created

---
+++ epicnotification/utils/responseOperators.go
@@ -0,0 +1,80 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+
+	"epicnotification/epicnotification/helpers"
+)
+
+type Sanitized struct {
+	Name          string `json:"name"`
+	EffectiveDate string `json:"effectiveData"`
+	OfferType     string `json:"offerType"`
+	StartDate     string `json:"startDate"`
+	ProductLink   string `json:"productLink"`
+	OriginalPrice string `json:"originalPrice"`
+	DiscountPrice string `json:"discountPrice"`
+	Free          bool   `json:"Free"`
+}
+
+func ExtractData(response helpers.ApiResponse) []Sanitized {
+	// var sanitizedResponse Sanitized
+	elements := response.Data.Catalog.SearchStore.Elements
+
+	fmt.Println("type of elements:", reflect.TypeOf(elements))
+
+	var resp []Sanitized
+	var baseUrl string = "https://store.epicgames.com/en-US/p"
+
+	for index, val := range elements {
+
+		resp = append(resp, Sanitized{
+			Name:          val.Title,
+			EffectiveDate: isoToString(val.EffectiveDate),
+			OfferType:     val.OfferType,
+			StartDate:     startDate(index, response),
+			ProductLink:   createLink(index, baseUrl, response),
+			OriginalPrice: val.Price.TotalPrice.FmtPrice.OriginalPrice,
+			DiscountPrice: val.Price.TotalPrice.FmtPrice.DiscountPrice,
+			Free:          isFree(val.Price.TotalPrice.OriginalPrice, val.Price.TotalPrice.DiscountPrice),
+		})
+	}
+
+	fmt.Println("type of sanitized list", resp)
+	return resp
+}
+
+func isoToString(date time.Time) string {
+	return date.Format("2006-01-02 15:04:05")
+}
+
+func startDate(index int, d helpers.ApiResponse) string {
+	data := d.Data.Catalog.SearchStore.Elements[index]
+	return isoToString(data.ViewableDate)
+}
+
+func isFree(op int, dp int) bool {
+	return op == dp
+}
+
+func createLink(index int, baseURL string, d helpers.ApiResponse) string {
+	data := d.Data.Catalog.SearchStore.Elements[index]
+
+	var slug interface{}
+
+	if data.ProductSlug != nil {
+		slug = data.ProductSlug
+	} else {
+		slug = data.OfferMappings[0].PageSlug
+	}
+
+	if str, ok := slug.(string); ok {
+		fmt.Println("converted slug is here", str)
+		asd := baseURL + "/" + str
+		return asd
+	} else {
+		return baseURL
+	}
+}
