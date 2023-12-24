f9fd6d2e9a636c0ba995112dbc1f76312267e105
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-12-24 13:17:34 +0300

    main.go module adapted to new version

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"epicnotification/epicnotification/apps/scrape"
-	"epicnotification/epicnotification/utils"
+	"epicnotification/epicnotification/routes"
 )
-
-// type T struct {
-// 	HomeRoute string `json:"homeRoute"`
-// 	SubRoute  string `json:"subRoute"`
-// }
 
 func main() {
 	// definitions
@@ -23,52 +16,11 @@ func main() {
 	fmt.Println("available routes: http://localhost:8080/scrape/epicgames")
 
 	// routes
-	http.HandleFunc("/", home)
-	http.HandleFunc("/scrape/epicgames", scrapeEndpoint)
+	http.HandleFunc("/", routes.HomeRoute)
+	http.HandleFunc("/scrape/epic", routes.EpicGamesRoute)
 
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Println("ERRROOROROR", err)
 	}
-
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	scrapeRoutes := map[string]interface{}{
-		"EpicGames": "http://localhost:8080/scrape/epicgames",
-	}
-	routes := map[string]interface{}{
-		"home":   "http://localhost:8080",
-		"scrape": scrapeRoutes,
-	}
-
-	jsonData, err := json.Marshal(routes)
-	if err != nil {
-		http.Error(w, "Json'a donusturme patladi", http.StatusInternalServerError)
-		return
-	}
-	w.Write(jsonData)
-}
-
-func scrapeEndpoint(w http.ResponseWriter, r *http.Request) {
-
-	var url = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=TR&allowCountries=TR"
-	res, err := scrape.Scraper(url) // get response as string
-	if err != nil {
-		fmt.Println(
-			fmt.Errorf("error in scraper response: %s", err),
-		)
-	}
-
-	// do extract
-	extracted := utils.ExtractData(res)
-
-	jsonData, err := json.Marshal(extracted)
-	if err != nil {
-		http.Error(w, "Error while converting response", http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(jsonData)
-
 }
