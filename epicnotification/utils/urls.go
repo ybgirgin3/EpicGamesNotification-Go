dcbad80ddcee391ada87646bdba3746fb14c73ea
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-12-24 14:31:00 +0300

    seperated urls module for the backend urls and base urls for each retailers

---
+++ epicnotification/utils/urls.go
@@ -0,0 +1,9 @@
+package utils
+
+var BaseURLs = map[string]string{
+	"epic-games": "https://store.epicgames.com/en-US/p",
+}
+
+var BackendURLs = map[string]string{
+	"epic-games": "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=TR&allowCountries=TR",
+}
