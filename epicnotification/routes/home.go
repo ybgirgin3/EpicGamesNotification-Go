1d8183f629cde5d1aa87463c7dcfbec489d41cb6
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-12-24 13:17:19 +0300

    home route created

---
+++ epicnotification/routes/home.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func HomeRoute(w http.ResponseWriter, r *http.Request) {
+	scrapeRoutes := map[string]interface{}{
+		"EpicGames": "http://localhost:8080/scrape/epicgames",
+	}
+	routes := map[string]interface{}{
+		"home":   "http://localhost:8080",
+		"scrape": scrapeRoutes,
+	}
+
+	jsonData, err := json.Marshal(routes)
+	if err != nil {
+		http.Error(w, "Json'a donusturme patladi", http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
+}
